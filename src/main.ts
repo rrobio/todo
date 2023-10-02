f10a5ee38a1ba1ebdec58037eae32e1a4c7d1826
robi <orlandinirobi@gmail.com>
2023-10-02 13:56:04 +0200

    convert src/ to typescript

--- src/main.ts
+++ src/main.ts
@@ -4,64 +4,67 @@ import * as td from './todo.js'
 const todos = td.loadTodoOrEmpty();
 
 if (todos.length === 0) {
-	todos.push(new Todo("test1"))
+    todos.push(td.newTodo("test1"));
 }
 
 function getFilter() {
-	return document.getElementById('filter-select').value
+    return (document.getElementById('filter-select') as HTMLSelectElement).value;
 }
 
-function renderTodos(todos) {
-    const filter = getFilter()
-    const app = document.querySelector('#app')
+function renderTodos(todos: Array<td.Todo>): void {
+    const filter = getFilter();
+    const app = document.querySelector('#app') as HTMLDivElement;
     app.replaceChildren();
     todos
-    	.filter((e) => {
-			switch(filter) {
-				case "all":
-					return true
-				case "done":
-					return e.done
-				case "skip":
-					return e.skip
-			}
-    	})
-    	.forEach((e) => {
-        const t = td.genereateNode(e)
-        app.appendChild(t)
-    })
+        .filter((e) => {
+            switch (filter) {
+                case "all":
+                    return true;
+                case "done":
+                    return e.done;
+                case "skip":
+                    return e.skip;
+            }
+        })
+        .forEach((e) => {
+            const t = td.genereateNode(e);
+            app.appendChild(t);
+        })
 }
 
-renderTodos(todos)
+renderTodos(todos);
 
-document.querySelector("#new-todo").addEventListener('click', (e) => {
-    let elem = document.querySelector("#new-todo-text");
-    const value = elem.value === ""?"empty":elem.value; 
+(document.querySelector("#new-todo") as HTMLButtonElement).addEventListener('click', () => {
+    let elem = document.querySelector("#new-todo-text") as HTMLTextAreaElement;
+    const value = elem.value === "" ? "empty" : elem.value;
     elem.value = "";
-	let nt = new td.Todo(value)
-	todos.push(nt)
-	renderTodos(todos)
-	td.saveTodo(todos)
-})
+    let nt = td.newTodo(value);
+    todos.push(nt);
+    renderTodos(todos);
+    td.saveTodo(todos);
+});
 
-document.getElementById('filter-select').addEventListener('change', (e) => renderTodos(todos))
+(document.getElementById('filter-select') as HTMLSelectElement).addEventListener('change', () => renderTodos(todos));
 
 document.addEventListener('deleteTodo', (e) => {
-    td.removeTodoByID(todos, e.target.value)
-	renderTodos(todos)
-	td.saveTodo(todos)
-})
+    const target = e.target as HTMLButtonElement;
+    td.removeTodoByID(todos, target.value);
+    renderTodos(todos);
+    td.saveTodo(todos);
+});
 
 document.addEventListener('toggleTodo', (e) => {
-    td.toggleTodoByID(todos, e.target.value)
-	renderTodos(todos)
-	td.saveTodo(todos)
+    const target = e.target as HTMLInputElement;
+    td.toggleTodoByID(todos, target.value);
+    renderTodos(todos);
+    td.saveTodo(todos);
 })
 
 document.addEventListener('skipTodo', (e) => {
-    td.skipTodoByID(todos, e.target.value)
-    td.pushBackTodo(todos, e.target.value)
-	renderTodos(todos)
-	td.saveTodo(todos)
+    const target = e.target as HTMLInputElement;
+    td.skipTodoByID(todos, target.value);
+    td.pushBackTodo(todos, target.value);
+    renderTodos(todos);
+    td.saveTodo(todos);
 })
 //setupCounter(document.querySelector('#counter'))
