c4ed2e9679e3fe0a6d09f471c7fbb8b9b7aba2db
robi <orlandinirobi@gmail.com>
2023-10-02 13:56:04 +0200

    Dodat eslint i stylelint

--- src/main.ts
+++ src/main.ts
@@ -1,70 +1,70 @@
 import './style.css'
 import * as td from './todo.js'
 
-const todos = td.loadTodoOrEmpty();
+const todos = td.loadTodoOrEmpty()
 
 if (todos.length === 0) {
-    todos.push(td.newTodo("test1"));
+  todos.push(td.newTodo('test1'))
 }
 
-function getFilter() {
-    return (document.getElementById('filter-select') as HTMLSelectElement).value;
+function getFilter (): string {
+  return (document.getElementById('filter-select') as HTMLSelectElement).value
 }
 
-function renderTodos(todos: Array<td.Todo>): void {
-    const filter = getFilter();
-    const app = document.querySelector('#app') as HTMLDivElement;
-    app.replaceChildren();
-    todos
-        .filter((e) => {
-            switch (filter) {
-                case "all":
-                    return true;
-                case "done":
-                    return e.done;
-                case "skip":
-                    return e.skip;
-            }
-        })
-        .forEach((e) => {
-            const t = td.genereateNode(e);
-            app.appendChild(t);
-        })
+function renderTodos (todos: td.Todo[]): void {
+  const filter = getFilter()
+  const app = document.querySelector('#app') as HTMLDivElement
+  app.replaceChildren()
+  todos
+    .filter((e) => {
+      switch (filter) {
+        case 'done':
+          return e.done
+        case 'skip':
+          return e.skip
+        default:
+          return true
+      }
+    })
+    .forEach((e) => {
+      const t = td.genereateNode(e)
+      app.appendChild(t)
+    })
 }
 
 renderTodos(todos);
 
-(document.querySelector("#new-todo") as HTMLButtonElement).addEventListener('click', () => {
-    let elem = document.querySelector("#new-todo-text") as HTMLTextAreaElement;
-    const value = elem.value === "" ? "empty" : elem.value;
-    elem.value = "";
-    let nt = td.newTodo(value);
-    todos.push(nt);
-    renderTodos(todos);
-    td.saveTodo(todos);
+(document.querySelector('#new-todo') as HTMLButtonElement).addEventListener('click', () => {
+  const elem = document.querySelector('#new-todo-text') as HTMLTextAreaElement
+  const value = elem.value === '' ? 'empty' : elem.value
+  elem.value = ''
+  const nt = td.newTodo(value)
+  todos.push(nt)
+  renderTodos(todos)
+  td.saveTodo(todos)
 });
 
-(document.getElementById('filter-select') as HTMLSelectElement).addEventListener('change', () => renderTodos(todos));
+(document.getElementById('filter-select') as HTMLSelectElement).addEventListener('change', () => { renderTodos(todos) })
 
 document.addEventListener('deleteTodo', (e) => {
-    const target = e.target as HTMLButtonElement;
-    td.removeTodoByID(todos, target.value);
-    renderTodos(todos);
-    td.saveTodo(todos);
-});
+  const target = e.target as HTMLButtonElement
+  td.removeTodoByID(todos, target.value)
+  renderTodos(todos)
+  td.saveTodo(todos)
+})
 
 document.addEventListener('toggleTodo', (e) => {
-    const target = e.target as HTMLInputElement;
-    td.toggleTodoByID(todos, target.value);
-    renderTodos(todos);
-    td.saveTodo(todos);
+  const target = e.target as HTMLInputElement
+  td.toggleTodoByID(todos, target.value)
+  renderTodos(todos)
+  td.saveTodo(todos)
 })
 
 document.addEventListener('skipTodo', (e) => {
-    const target = e.target as HTMLInputElement;
-    td.skipTodoByID(todos, target.value);
-    td.pushBackTodo(todos, target.value);
-    renderTodos(todos);
-    td.saveTodo(todos);
+  const target = e.target as HTMLInputElement
+  td.skipTodoByID(todos, target.value)
+  td.pushBackTodo(todos, target.value)
+  renderTodos(todos)
+  td.saveTodo(todos)
 })
-//setupCounter(document.querySelector('#counter'))
+// setupCounter(document.querySelector('#counter'))
--- src/todo.ts
+++ src/todo.ts
@@ -1,128 +1,127 @@
-export type ID = string;
+export type ID = string
 
-export type Todo = {
-    text: string;
-    done: boolean;
-    skip: boolean;
-    id: ID;
+export interface Todo {
+  text: string
+  done: boolean
+  skip: boolean
+  id: ID
 }
 
-export function newTodo(t: string): Todo {
-    return {
-        text: t,
-        done: false,
-        skip: false,
-        id: crypto.randomUUID(),
-    } as Todo;
+export function newTodo (t: string): Todo {
+  return {
+    text: t,
+    done: false,
+    skip: false,
+    id: crypto.randomUUID()
+  }
 }
 
-export function genereateNode(todo: Todo) {
-    const todoNode = document.createElement("div");
-    const inputDone = document.createElement("input");
-    const labelDone = document.createElement("label");
-    const inputSkip = document.createElement("input");
-    const labelSkip = document.createElement("label");
-    const inputDelete = document.createElement("button");
-    const labelDelete = document.createElement("label");
-    const text = document.createElement("p");
+export function genereateNode (todo: Todo): HTMLDivElement {
+  const todoNode = document.createElement('div')
+  const inputDone = document.createElement('input')
+  const labelDone = document.createElement('label')
+  const inputSkip = document.createElement('input')
+  const labelSkip = document.createElement('label')
+  const inputDelete = document.createElement('button')
+  const labelDelete = document.createElement('label')
+  const text = document.createElement('p')
 
-    todoNode.setAttribute('class', 'todo');
+  todoNode.setAttribute('class', 'todo')
 
-    inputDone.setAttribute('type', 'checkbox');
-    inputDone.setAttribute('value', todo.id);
-    inputDone.setAttribute('id', `done-button#${todo.id}`);
-    if (todo.done) {
-        inputDone.checked = true;
-    }
-    inputDone.addEventListener('click', (event) => {
-        event.preventDefault();
-        let toggleEvent = new Event('toggleTodo', { bubbles: true });
-        event.target!.dispatchEvent(toggleEvent);
-    })
-    labelDone.setAttribute('for', `done-button#${todo.id}`);
-    labelDone.textContent = "done";
+  inputDone.setAttribute('type', 'checkbox')
+  inputDone.setAttribute('value', todo.id)
+  inputDone.setAttribute('id', `done-button#${todo.id}`)
+  if (todo.done) {
+    inputDone.checked = true
+  }
+  inputDone.addEventListener('click', (event) => {
+    event.preventDefault()
+    const toggleEvent = new Event('toggleTodo', { bubbles: true })
+    event.target?.dispatchEvent(toggleEvent)
+  })
+  labelDone.setAttribute('for', `done-button#${todo.id}`)
+  labelDone.textContent = 'done'
 
-    inputSkip.setAttribute('type', 'checkbox');
-    inputSkip.setAttribute('value', todo.id);
-    inputSkip.setAttribute('id', `skip-button#${todo.id}`);
-    if (todo.skip) {
-        inputSkip.checked = true;
-    }
-    inputSkip.addEventListener('click', (event) => {
-        event.preventDefault();
-        let toggleEvent = new Event('skipTodo', { bubbles: true });
-        event.target!.dispatchEvent(toggleEvent);
-    })
-    labelSkip.setAttribute('for', `skip-button#${todo.id}`);
-    labelSkip.textContent = "skip";
+  inputSkip.setAttribute('type', 'checkbox')
+  inputSkip.setAttribute('value', todo.id)
+  inputSkip.setAttribute('id', `skip-button#${todo.id}`)
+  if (todo.skip) {
+    inputSkip.checked = true
+  }
+  inputSkip.addEventListener('click', (event) => {
+    event.preventDefault()
+    const toggleEvent = new Event('skipTodo', { bubbles: true })
+    event.target?.dispatchEvent(toggleEvent)
+  })
+  labelSkip.setAttribute('for', `skip-button#${todo.id}`)
+  labelSkip.textContent = 'skip'
 
-    inputDelete.setAttribute('type', 'button');
-    inputDelete.setAttribute('value', todo.id);
-    inputDelete.setAttribute('id', 'delete-button');
-    inputDelete.setAttribute('id', `delete-button#${todo.id}`);
-    inputDelete.innerHTML = "x";
-    inputDelete.addEventListener('click', function(event) {
-        event.preventDefault();
-        let deleteEvent = new Event('deleteTodo', { bubbles: true });
-        event.target!.dispatchEvent(deleteEvent);
-    })
-    labelDelete.setAttribute('for', `delete-button#${todo.id}`);
-    labelDelete.textContent = "delete";
+  inputDelete.setAttribute('type', 'button')
+  inputDelete.setAttribute('value', todo.id)
+  inputDelete.setAttribute('id', 'delete-button')
+  inputDelete.setAttribute('id', `delete-button#${todo.id}`)
+  inputDelete.innerHTML = 'x'
+  inputDelete.addEventListener('click', function (event) {
+    event.preventDefault()
+    const deleteEvent = new Event('deleteTodo', { bubbles: true })
+    event.target?.dispatchEvent(deleteEvent)
+  })
+  labelDelete.setAttribute('for', `delete-button#${todo.id}`)
+  labelDelete.textContent = 'delete'
 
-    text.textContent = todo.text;
+  text.textContent = todo.text
 
+  if (todo.done) inputSkip.disabled = true
 
-    if (todo.done) inputSkip.disabled = true;
+  todoNode.appendChild(inputDone)
+  todoNode.appendChild(labelDone)
+  todoNode.appendChild(inputSkip)
+  todoNode.appendChild(labelSkip)
+  todoNode.appendChild(inputDelete)
+  todoNode.appendChild(labelDelete)
+  todoNode.appendChild(text)
 
-    todoNode.appendChild(inputDone);
-    todoNode.appendChild(labelDone);
-    todoNode.appendChild(inputSkip);
-    todoNode.appendChild(labelSkip);
-    todoNode.appendChild(inputDelete);
-    todoNode.appendChild(labelDelete);
-    todoNode.appendChild(text);
-
-    return todoNode;
+  return todoNode
 }
 
-export function loadTodoOrEmpty(): Array<Todo> {
-    let todojson = localStorage.getItem('todos');
-    if (typeof todojson === "string") {
-        return JSON.parse(todojson);
-    }
-    return [];
+export function loadTodoOrEmpty (): Todo[] {
+  const todojson = localStorage.getItem('todos')
+  if (typeof todojson === 'string') {
+    return JSON.parse(todojson)
+  }
+  return []
 }
 
-function getIndexByID(todos: Array<Todo>, id: ID): number {
-    return todos.findIndex((e) => {
-        return e.id === id;
-    })
+function getIndexByID (todos: Todo[], id: ID): number {
+  return todos.findIndex((e) => {
+    return e.id === id
+  })
 }
 
-export function saveTodo(todos: Array<Todo>) {
-    let todoJSON = JSON.stringify(todos);
-    localStorage.setItem('todos', todoJSON);
+export function saveTodo (todos: Todo[]): void {
+  const todoJSON = JSON.stringify(todos)
+  localStorage.setItem('todos', todoJSON)
 }
 
-export function skipTodoByID(todos: Array<Todo>, id: ID) {
-    const i = getIndexByID(todos, id);
-    if (i >= 0) todos[i].skip = !todos[i].skip;
+export function skipTodoByID (todos: Todo[], id: ID): void {
+  const i = getIndexByID(todos, id)
+  if (i >= 0) todos[i].skip = !todos[i].skip
 }
 
-export function toggleTodoByID(todos: Array<Todo>, id: ID) {
-    const i = getIndexByID(todos, id);
-    if (i >= 0) todos[i].done = !todos[i].done;
+export function toggleTodoByID (todos: Todo[], id: ID): void {
+  const i = getIndexByID(todos, id)
+  if (i >= 0) todos[i].done = !todos[i].done
 }
 
-export function removeTodoByID(todos: Array<Todo>, id: ID) {
-    const i = getIndexByID(todos, id);
-    if (i >= 0) todos.splice(i, 1);
+export function removeTodoByID (todos: Todo[], id: ID): void {
+  const i = getIndexByID(todos, id)
+  if (i >= 0) todos.splice(i, 1)
 }
 
-export function pushBackTodo(todos: Array<Todo>, id: ID) {
-    const i = getIndexByID(todos, id);
+export function pushBackTodo (todos: Todo[], id: ID): void {
+  const i = getIndexByID(todos, id)
 
-    if (i >= 0) {
-        todos.push(...todos.splice(i, 1));
-    }
+  if (i >= 0) {
+    todos.push(...todos.splice(i, 1))
+  }
 }
