b695c897bf2a5d92856cbf076d66e00d91d1202c
robi <orlandinirobi@gmail.com>
2023-10-03 14:26:22 +0200

    koristit faker.js za testove

--- src/todo.spec.ts
+++ src/todo.spec.ts
@@ -1,26 +1,40 @@
-import { Todo } from './model/todo.ts'
+import { Todo, TodoData } from './model/todo.ts'
+import { faker } from '@faker-js/faker';
+
+function getRandomTodoData(): TodoData {
+	return {
+		text: faker.lorem.lines(2),
+		done: faker.datatype.boolean(),
+		skip: faker.datatype.boolean(),
+		id: +faker.date.anytime(),
+	}
+}
 
 describe('new Todo', () => {
   test('should create new todo', () => {
-    const todo = new Todo('test')
+    const data = getRandomTodoData()
+    const todo = new Todo(data.text)
     expect(todo).toBeInstanceOf(Todo)
-    expect(todo.text).toBe('test')
+    expect(todo.text).toBe(data.text)
     expect(todo.id).not.toBeNull()
   })
 })
 describe('Todo', () => {
   test('should create new todo', () => {
-    const todo = new Todo('test')
+    const data = getRandomTodoData()
+    const todo = new Todo(data.text)
     expect(todo).toBeInstanceOf(Todo)
   })
   test('should toggle done state', () => {
-    const todo = new Todo('test')
+    const data = getRandomTodoData()
+    const todo = new Todo(data.text)
     expect(todo.done).toBeFalsy()
     todo.toggleDone()
     expect(todo.done).toBeTruthy()
   })
   test('should toggle skip state', () => {
-    const todo = new Todo('test')
+    const data = getRandomTodoData()
+    const todo = new Todo(data.text)
     expect(todo.skip).toBeFalsy()
     todo.toggleSkip()
     expect(todo.skip).toBeTruthy()
@@ -28,7 +42,8 @@ describe('Todo', () => {
 })
 describe('recreate todo', () => {
   test('should create new todo', () => {
-    const todo1 = new Todo('test')
+    const data = getRandomTodoData()
+    const todo1 = new Todo(data.text)
     expect(todo1).toBeInstanceOf(Todo)
     todo1.toggleDone()
     const todo2 = Todo.clone(todo1.text, todo1.done, todo1.skip, todo1.id)
@@ -38,7 +53,8 @@ describe('recreate todo', () => {
     expect(todo2.id).toBe(todo1.id)
   })
   test('done state should not be equal', () => {
-    const todo1 = new Todo('test')
+    const data = getRandomTodoData()
+    const todo1 = new Todo(data.text)
     expect(todo1).toBeInstanceOf(Todo)
     const todo2 = Todo.clone(todo1.text, todo1.done, todo1.skip, todo1.id)
     todo1.toggleDone()
@@ -50,34 +66,43 @@ describe('recreate todo', () => {
 })
 describe('export todo', () => {
   test('should export todo into a tododata object', () => {
-    const todo = new Todo('test', 12345)
-    expect(todo.export().text).toBe('test')
-    expect(todo.export().id).toBe(12345)
+    const data = getRandomTodoData()
+    const todo = Todo.import(data)
+    expect(todo.export().text).toBe(data.text)
+    expect(todo.export().id).toBe(data.id)
   })
   test('export to json', () => {
-    const todo = new Todo('test', 12345)
-    expect(JSON.stringify(todo.export())).toBe('{"text":"test","done":false,"skip":false,"id":12345}')
+    const data = getRandomTodoData()
+    const todo = Todo.import(data)
+    expect(JSON.stringify(todo.export())).toBe(JSON.stringify(data))
   })
   test('export to json (modified)', () => {
-    const todo = new Todo('test', 12345)
+    const data = getRandomTodoData()
+    const todo = Todo.import(data)
+    expect(JSON.stringify(todo.export())).toBe(JSON.stringify(data))
     todo.toggleDone()
-    expect(JSON.stringify(todo.export())).toBe('{"text":"test","done":true,"skip":false,"id":12345}')
+    expect(JSON.stringify(todo.export())).not.toBe(JSON.stringify(data))
   })
 })
 describe('import todo', () => {
   test('should import todo from tododata', () => {
-    const todo = Todo.import({ text: 'test', id: 12345, done: false, skip: false })
-    expect(todo.text).toBe('test')
-    expect(todo.id).toBe(12345)
+    const data = getRandomTodoData()
+    const todo = Todo.import(data)
+    expect(todo.text).toBe(data.text)
+    expect(todo.id).toBe(data.id)
+    expect(todo.done).toBe(data.done)
+    expect(todo.skip).toBe(data.skip)
   })
   test('import from json', () => {
-    const todo = Todo.import(JSON.parse('{"text":"test1","done":false,"skip":false,"id":12345}'))
-    expect(todo.text).toBe('test1')
-    expect(todo.id).toBe(12345)
+    const data = getRandomTodoData()
+    const todo = Todo.import(JSON.parse(JSON.stringify(data)))
+    expect(todo.text).toBe(data.text)
+    expect(todo.id).toBe(data.id)
     expect(todo).toBeInstanceOf(Todo)
   })
   test('should be equal when importing from export', () => {
-    const todo1 = new Todo('test', 12345)
+    const data = getRandomTodoData()
+    const todo1 = Todo.import(data)
     const todo2 = Todo.import(JSON.parse(JSON.stringify(todo1.export())))
     expect(todo2).toBeInstanceOf(Todo)
     expect(todo2.text).toBe(todo1.text)
