8df4d84c23ec0617ed9f0188234bd852e5d78509
robi <orlandinirobi@gmail.com>
2023-10-03 15:05:09 +0200

    refaktor jednakost u testiranju

--- src/todo.spec.ts
+++ src/todo.spec.ts
@@ -1,13 +1,13 @@
-import { Todo, TodoData } from './model/todo.ts'
-import { faker } from '@faker-js/faker';
+import { Todo, type TodoData } from './model/todo.ts'
+import { faker } from '@faker-js/faker'
 
-function getRandomTodoData(): TodoData {
-	return {
-		text: faker.lorem.lines(2),
-		done: faker.datatype.boolean(),
-		skip: faker.datatype.boolean(),
-		id: +faker.date.anytime(),
-	}
+function getRandomTodoData (): TodoData {
+  return {
+    text: faker.lorem.lines(2),
+    done: faker.datatype.boolean(),
+    skip: faker.datatype.boolean(),
+    id: +faker.date.anytime()
+  }
 }
 
 describe('new Todo', () => {
@@ -41,16 +41,13 @@ describe('Todo', () => {
   })
 })
 describe('recreate todo', () => {
-  test('should create new todo', () => {
+  test('should clone todo', () => {
     const data = getRandomTodoData()
     const todo1 = new Todo(data.text)
     expect(todo1).toBeInstanceOf(Todo)
     todo1.toggleDone()
     const todo2 = Todo.clone(todo1.text, todo1.done, todo1.skip, todo1.id)
-    expect(todo2.text).toBe(todo1.text)
-    expect(todo2.done).toBe(todo1.done)
-    expect(todo2.skip).toBe(todo1.skip)
-    expect(todo2.id).toBe(todo1.id)
+    expect(todo1.export()).toStrictEqual(todo2.export())
   })
   test('done state should not be equal', () => {
     const data = getRandomTodoData()
@@ -88,10 +85,7 @@ describe('import todo', () => {
   test('should import todo from tododata', () => {
     const data = getRandomTodoData()
     const todo = Todo.import(data)
-    expect(todo.text).toBe(data.text)
-    expect(todo.id).toBe(data.id)
-    expect(todo.done).toBe(data.done)
-    expect(todo.skip).toBe(data.skip)
+    expect(todo.export()).toEqual(data)
   })
   test('import from json', () => {
     const data = getRandomTodoData()
@@ -105,9 +99,6 @@ describe('import todo', () => {
     const todo1 = Todo.import(data)
     const todo2 = Todo.import(JSON.parse(JSON.stringify(todo1.export())))
     expect(todo2).toBeInstanceOf(Todo)
-    expect(todo2.text).toBe(todo1.text)
-    expect(todo2.done).toBe(todo1.done)
-    expect(todo2.skip).toBe(todo1.skip)
-    expect(todo2.id).toBe(todo1.id)
+    expect(todo2.export()).toStrictEqual(todo1.export())
   })
 })
